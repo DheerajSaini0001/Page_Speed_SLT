368436146a87c67d6715c6db5bee27f6130dbd8d
Dheeraj Saini <dheerajsaini131652@gmail.com>
2025-09-16 10:09:08 +0530

    COlour being  normal

--- Frontend/src/Component/Sidebar.jsx
+++ Frontend/src/Component/Sidebar.jsx
@@ -86,7 +86,7 @@ export default function Sidebar({ children ,data}) {
       >
         {/* Logo */}
         <div className="  flex flex-col justify-center items-center text-2xl py-4 border-b border-gray-700">
-          <a href="#deshboard" className="text-4xl font-bold bg-gradient-to-r from-yellow-500 via-green-500 to-red-500 bg-clip-text text-transparent  ">Result</a>
+          <a href="#deshboard" className="text-4xl font-bold bg-green-600 ">Result</a>
         </div>
 
         {/* Menu */}
