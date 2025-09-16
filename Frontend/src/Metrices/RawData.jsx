21c0a434441198938be656409e4824f603eaa440
Dheeraj Saini <dheerajsaini131652@gmail.com>
2025-09-16 10:17:32 +0530

    Raw Data set

--- Frontend/src/Metrices/RawData.jsx
+++ Frontend/src/Metrices/RawData.jsx
@@ -103,7 +103,7 @@ const renderData = (obj, parentKey = "") => {
                  shadow-lg hover:shadow-2xl transform hover:-translate-y-1 
                  transition-all duration-300 m-4 flex flex-col items-center 
                  justify-start p-6 space-y-6 ">
-     <h1 className="text-3xl font-extrabold text-amber-100 mb-6">
+     <h1 className="text-3xl font-extrabold text-white mb-6">
         Raw Data </h1>
 
       <div className="w-full max-w-4xl p-6 rounded-2xl shadow-lg border-l-4 border-indigo-500 bg-gradient-to-br from-blue-900 via-gray-900 to-black
