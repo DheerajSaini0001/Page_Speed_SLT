3408f3d4cc6f59f642c2cc1c9492718cace14b04
Piyush Saini <piyushsaini1226@gmail.com>
2025-09-18 12:36:33 +0530

    all done

---
+++ Backend/Main/main.js
@@ -0,0 +1,32 @@
+import MetricesCalculation from "../Calculation/MetricesCalculation.js";
+import Metrices from "../Data/Metrices.js";
+import OverAll from "../Data/OverAll.js";
+import axios_cheerio from "../Tools/axios_cheerio.js";
+import googleAPI from "../Tools/googleAPI.js";
+import robotsRes from "../Tools/robotsRes.js";
+
+export default async function main(message) {
+
+  var url = message[0].trim();
+  // If it doesn't start with http:// or https://, add https://
+  if (!/^https?:\/\//i.test(url)) {
+    url = "https://" + url;
+  }
+
+    const googleApi_Data = await googleAPI(url);
+    const axios_cheerio_Data = await axios_cheerio(url);
+    const robotsRes_Data = await robotsRes(url);
+
+    const MetricesCalculation_Data = await MetricesCalculation(url,googleApi_Data,axios_cheerio_Data,robotsRes_Data)
+    const Metrices_Data = await Metrices(url,MetricesCalculation_Data.technicalReport,MetricesCalculation_Data.seoReport,MetricesCalculation_Data.accessibilityReport,MetricesCalculation_Data.securityReport,MetricesCalculation_Data.uxReport,MetricesCalculation_Data.conversionReport,MetricesCalculation_Data.aioReport)
+
+    const Overall_Data = OverAll(Metrices_Data)
+
+    return {
+        Metrices_Data,
+        Overall_Data
+
+    }
+
+
+}
--- Backend/Server.js
+++ Backend/Server.js
@@ -1,11 +1,6 @@
 import express from "express";
 import cors from "cors";
-import googleAPI from "./Tools/googleAPI.js";
-import axios_cheerio from "./Tools/axios_cheerio.js";
-import robotsRes from "./Tools/robotsRes.js";
-import MetricesCalculation from "./Calculation/MetricesCalculation.js";
-import Metrices from "./Data/Metrices.js";
-import OverAll from "./Data/OverAll.js";
+import main from "./Main/main.js";
 
 const PORT =2000;
 
@@ -16,27 +11,16 @@ app.use(cors());
 app.post('/data', async (req, res) => {
 
   const  message  = req.body;
-
-  var url = message[0].trim();
-  // If it doesn't start with http:// or https://, add https://
-  if (!/^https?:\/\//i.test(url)) {
-    url = "https://" + url;
-  }
-
-  console.log(`URL Received: ${url}`);
+  console.log(`URL Received: ${message[0]}`);
 
   try {
-    const googleApi_Data = await googleAPI(url);
-    const axios_cheerio_Data = await axios_cheerio(url);
-    const robotsRes_Data = await robotsRes(url);
-
-    const MetricesCalculation_Data = await MetricesCalculation(url,googleApi_Data,axios_cheerio_Data,robotsRes_Data)
-    const Metrices_Data = await Metrices(url,MetricesCalculation_Data.technicalReport,MetricesCalculation_Data.seoReport,MetricesCalculation_Data.accessibilityReport,MetricesCalculation_Data.securityReport,MetricesCalculation_Data.uxReport,MetricesCalculation_Data.conversionReport,MetricesCalculation_Data.aioReport)
-
-    const Overall_Data = OverAll(Metrices_Data)
-  res.json({Metrices_Data,Overall_Data});
-  console.log(Metrices_Data);
-  console.log(Overall_Data);
+  const data = await main(message)
+  res.json({
+  Metrices_Data: data.Metrices_Data,
+  Overall_Data: data.Overall_Data
+});
+  console.log(data.Metrices_Data);
+  console.log(data.Overall_Data);
     
   } catch (error) {
     console.error("Error fetching PageSpeed data:", error);
