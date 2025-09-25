4d2c7d1ed63a848b017bca161f1c73c6a2a700f1
Piyush Saini <piyushsaini1226@gmail.com>
2025-09-25 10:31:37 +0530

    On Page SEO Done

--- Backend/Data/Metrices.js
+++ Backend/Data/Metrices.js
@@ -1,126 +1,154 @@
 
-export default function Metrices(url,technicalReport,seoReport,accessibilityReport,securityReport,uxReport,conversionReport,aioReport) {
+export default function Metrices(url, MetricesCalculation_Data, Overall_Data) {
 
-    const Metrices = {
-      URL:url,
-      Technical_Performance:{
-        Core_Web_Vitals:{
-          LCP_Score:technicalReport.lcpScore,
-          CLS_Score:technicalReport.clsScore,
-          INP_Score:technicalReport.inpScore,
-          Total_Score_A1:technicalReport.total_A1
-        },
-        Delivery_and_Render:{
-          TTFB_Score:technicalReport.ttfbScore,
-          Compression_Score:technicalReport.compressionScore,
-          Caching_Score:technicalReport.cachingscore,
-          HTTP_Score:technicalReport.httpscore,
-          Total_Score_A2:technicalReport.total_A2
-        },
-        Crawlability_and_Hygiene:{
-          Sitemap_Score:technicalReport.sitemapScore,
-          Robots_Score:technicalReport.robotsScore,
-          Broken_Links_Score:technicalReport.brokenLinksScore,
-          Redirect_Chains_Score:technicalReport.redirectChainsScore,
-          Total_Score_A3:technicalReport.totalScore_A3
-        },
-        Technical_Performance_Score_Total:technicalReport.totalScore
+  const metrices = {
+    Schema:MetricesCalculation_Data.aioReport.G.jsonLdScripts,
+    Site: url,
+    Score: Overall_Data.totalScore,
+    Grade: Overall_Data.grade,
+    AIO_Compatibility_Badge: MetricesCalculation_Data.aioReport.G.aioCompatibleBadge,
+    Section_Score: Overall_Data.sectionScores,
+    Top_Fixes: Overall_Data.topFixes,
+    recommendations: Overall_Data.recommendations,
+    Technical_Performance: {
+      Core_Web_Vitals: {
+        LCP_Score: MetricesCalculation_Data.technicalReport.lcpScore,
+        CLS_Score: MetricesCalculation_Data.technicalReport.clsScore,
+        INP_Score: MetricesCalculation_Data.technicalReport.inpScore,
+        Total_Score_A1: MetricesCalculation_Data.technicalReport.total_A1,
       },
-       On_Page_SEO:{
-        Essentials:{
-          Unique_Title_Score:{
-            title: seoReport.B1.title,
-            titleLength: seoReport.B1.titleLength,
-            titleScore: seoReport.B1.titleScore,
-          },
-          Meta_Description_Score:{
-            metaDescription: seoReport.B1.metaDesc,
-            metaDescLength: seoReport.B1.metaDescLength,
-            metaDescScore: seoReport.B1.metaDescScore
-          },
-          Canonical_Score:{
-            canonical: seoReport.B1.canonical,
-            canonicalScore: seoReport.B1.canonicalScore
-          },
-          H1_Score:{
-            h1Count:seoReport.B1.h1Count,
-            h1Score:seoReport.B1.h1Score
-          },
-          Total_Score_B1:seoReport.B1.total
-        },
-        Media_and_Semantics:{
-          Image_ALT_Score:seoReport.B2.imageAltScore,
-          Heading_Hierarchy_Score:seoReport.B2.hierarchyScore,
-          Descriptive_Links_Score:seoReport.B2.linkScore,
-          Total_Score_B2:seoReport.B2.total
-        },
-        Structure_and_Uniqueness:{
-          URL_Slugs_Score:seoReport.B3.urlSlugScore,
-          Duplicate_Content_Score:seoReport.B3.duplicateContent,
-          Pagination_Tags_Score:seoReport.B3.paginationScore,
-          Total_Score_B3:seoReport.B3.total
-        },
-        On_Page_SEO_Score_Total:seoReport.totalSEO
+      Delivery_and_Render: {
+        TTFB_Score: MetricesCalculation_Data.technicalReport.ttfbScore,
+        Compression_Score: MetricesCalculation_Data.technicalReport.compressionScore,
+        Caching_Score: MetricesCalculation_Data.technicalReport.cachingscore,
+        HTTP_Score: MetricesCalculation_Data.technicalReport.httpscore,
+        Total_Score_A2: MetricesCalculation_Data.technicalReport.total_A2,
       },
-      Accessibility:{
-        Color_Contrast_Score:accessibilityReport.C.colorContrast,
-        Focusable_Score:accessibilityReport.C.keyboardNavigation,
-        ARIA_Score:accessibilityReport.C.ariaLabeling,
-        Alt_or_Text_Equivalents_Score:accessibilityReport.C.altTextEquivalents,
-        Skip_Links_or_Landmarks_Score:accessibilityReport.C.skipLinksLandmarks,
-        Accessibility_Score_Total:accessibilityReport.C.totalCScore
+      Crawlability_and_Hygiene: {
+        Sitemap_Score: MetricesCalculation_Data.technicalReport.sitemapScore,
+        Robots_Score: MetricesCalculation_Data.technicalReport.robotsScore,
+        Broken_Links_Score: MetricesCalculation_Data.technicalReport.brokenLinksScore,
+        Redirect_Chains_Score: MetricesCalculation_Data.technicalReport.redirectChainsScore,
+        Total_Score_A3: MetricesCalculation_Data.technicalReport.totalScore_A3,
       },
-      Security_or_Compliance:{
-        HTTPS_Score:securityReport.D.httpsMixedContent,
-        HSTS_Score:securityReport.D.hsts,
-        Security_Headers_Score:securityReport.D.securityHeaders,
-        Cookie_Banner_and_Consent_Mode_Score:securityReport.D.cookieConsent,
-        Error_Pages_Score:securityReport.D.errorPages,
-        Security_or_Compliance_Score_Total:securityReport.D.totalDScore
+      Technical_Performance_Score_Total: MetricesCalculation_Data.technicalReport.totalScore,
+    },
+    On_Page_SEO: {
+      Essentials: {
+        Unique_Title: {
+          Title: MetricesCalculation_Data.seoReport.B1.title,
+          Parameter:'Length of Title must be in between 30-60 characters',
+          Title_Length: MetricesCalculation_Data.seoReport.B1.titleLength,
+          Score: MetricesCalculation_Data.seoReport.B1.titleScore,
+        },
+        Meta_Description: {
+          MetaDescription: MetricesCalculation_Data.seoReport.B1.MetaDescription,
+          Parameter:'Length of MetaDescription must be less than 160 characters',
+          MetaDescription_Length: MetricesCalculation_Data.seoReport.B1.metaDescLength,
+          Score: MetricesCalculation_Data.seoReport.B1.metaDescScore,
+        },
+        Canonical: {
+          Canonical: MetricesCalculation_Data.seoReport.B1.canonical,
+          Parameter:'Page URL must be same as Canonical tag href URL',
+          Score: MetricesCalculation_Data.seoReport.B1.canonicalScore,
+        },
+        H1: {
+          H1_Count: MetricesCalculation_Data.seoReport.B1.h1Count,
+          Parameter:'There must be only one H1 tag on the Page',
+          Score: MetricesCalculation_Data.seoReport.B1.h1Score,
+        },
+        Total_Score_B1: MetricesCalculation_Data.seoReport.B1.total,
       },
-      UX_and_Content_Structure:{
-        Mobile_Friendliness_Score:uxReport.E.mobileFriendliness,
-        Navigation_Depth_Score:uxReport.E.navigationDepth,
-        Layout_Shift_On_interactions_Score:uxReport.E.layoutShift,
-        Readability_Score:uxReport.E.readability,
-        Intrusive_Interstitials_Score:uxReport.E.intrusiveInterstitials,
-        UX_and_Content_Structure_Score_Total:uxReport.E.totalEScore
+      Media_and_Semantics: {
+        Image_ALT:{
+          Parameter:'Only Image which consist alt attribute must be Meaningfull !("", "image", "logo", "icon","pic","picture","photo"," ","12345","-","graphics")',
+          Score:MetricesCalculation_Data.seoReport.B2.imageAltScore
+        },
+        Heading_Hierarchy:{
+          Parameter:'Must follow heading hierarcy h1->h2->h3',
+          Score:MetricesCalculation_Data.seoReport.B2.hierarchyScore
+        },
+        Descriptive_Links:{
+          Parameter:'anchor tag text must be meaningfull !("click here", "read more","learn more","details","link","more","go","this")',
+          Score: MetricesCalculation_Data.seoReport.B2.linkScore
+        },
+        Total_Score_B2: MetricesCalculation_Data.seoReport.B2.total,
       },
-      Conversion_and_Lead_Flow:{
-        Primary_CTAs_Score:conversionReport.F.primaryCTA,
-        Forms_Score:conversionReport.F.forms,
-        Thank_You_or_Success_State_Score:conversionReport.F.thankYouState,
-        Tracking_Of_Form_Submits_or_Events_Score:conversionReport.F.tracking,
-        Contact_Info_Score:conversionReport.F.contactInfo,
-        Load_On_CRM_or_Webhook_Score:conversionReport.F.crmWebhook,
-        Conversion_and_Lead_Flow_Score_Total:conversionReport.F.totalFScore
+      Structure_and_Uniqueness: {
+         URL_Slugs:{
+          Parameter:'Slug must be meanoingfull & length must be less than 75 characters ',
+          Score:MetricesCalculation_Data.seoReport.B3.urlSlugScore
+        },
+        Duplicate_Content:{
+          Parameter:' Duplicate contnent occured within the page less than 50 %',
+          Score: MetricesCalculation_Data.seoReport.B3.duplicateContent
+        },
+        Pagination_Tags:{
+          Parameter:'Check for link rel to next and prev',
+          Score: MetricesCalculation_Data.seoReport.B3.paginationScore
+        },
+        Total_Score_B3: MetricesCalculation_Data.seoReport.B3.total,
       },
-      AIO_Readiness:{
-        Entity_and_Organization_Clarity:{
-          Organization_JSON_LD_Score:aioReport.G.orgFields,
-          Consistent_NAP_Score:aioReport.G.napConsistency,
-          Humans_or_Policies_Score:aioReport.G.policies,
-          Total_Score_G1:aioReport.G.totalG1
-        },
-        Content_Answerability_and_Structure:{
-          FAQ_or_How_To_JSON_LD_Score:aioReport.G.faqJsonLd,
-          Section_Anchors_or_TOC_Score:aioReport.G.sectionAnchors,
-          Descriptive_Media_Captions_or_Figcaptions_Score:aioReport.G.mediaCaptions,
-          Total_Score_G2: aioReport.G.totalG2
-        },
-         Product_or_Inventory_Schema_and_Feeds:{
-          Correct_Schema_Types_Score:aioReport.G.productSchemas,
-          Feed_Availability_Score:aioReport.G.feedAvailability,
-          Total_Score_G3: aioReport.G.totalG3
-        },
-         Crawl_Friendliness_for_Knowledge_Agents:{
-          Robots_Allowlist_Score: aioReport.G.crawlFriendliness,
-          Total_Score_G4: aioReport.G.crawlFriendliness
-        },
-        AIO_Readiness_Score_Total: aioReport.G.totalGScore,
-        AIO_Compatibility_Badge: aioReport.G.aioCompatibleBadge
-      }
+      On_Page_SEO_Score_Total: MetricesCalculation_Data.seoReport.totalSEO,
+    },
+    Accessibility: {
+      Color_Contrast_Score: MetricesCalculation_Data.accessibilityReport.C.colorContrast,
+      Focusable_Score: MetricesCalculation_Data.accessibilityReport.C.keyboardNavigation,
+      ARIA_Score: MetricesCalculation_Data.accessibilityReport.C.ariaLabeling,
+      Alt_or_Text_Equivalents_Score: MetricesCalculation_Data.accessibilityReport.C.altTextEquivalents,
+      Skip_Links_or_Landmarks_Score: MetricesCalculation_Data.accessibilityReport.C.skipLinksLandmarks,
+      Accessibility_Score_Total: MetricesCalculation_Data.accessibilityReport.C.totalCScore,
+    },
+    Security_or_Compliance: {
+      HTTPS_Score: MetricesCalculation_Data.securityReport.D.httpsMixedContent,
+      HSTS_Score: MetricesCalculation_Data.securityReport.D.hsts,
+      Security_Headers_Score: MetricesCalculation_Data.securityReport.D.securityHeaders,
+      Cookie_Banner_and_Consent_Mode_Score: MetricesCalculation_Data.securityReport.D.cookieConsent,
+      Error_Pages_Score: MetricesCalculation_Data.securityReport.D.errorPages,
+      Security_or_Compliance_Score_Total: MetricesCalculation_Data.securityReport.D.totalDScore,
+    },
+    UX_and_Content_Structure: {
+      Mobile_Friendliness_Score: MetricesCalculation_Data.uxReport.E.mobileFriendliness,
+      Navigation_Depth_Score: MetricesCalculation_Data.uxReport.E.navigationDepth,
+      Layout_Shift_On_interactions_Score: MetricesCalculation_Data.uxReport.E.layoutShift,
+      Readability_Score: MetricesCalculation_Data.uxReport.E.readability,
+      Intrusive_Interstitials_Score: MetricesCalculation_Data.uxReport.E.intrusiveInterstitials,
+      UX_and_Content_Structure_Score_Total: MetricesCalculation_Data.uxReport.E.totalEScore,
+    },
+    Conversion_and_Lead_Flow: {
+      Primary_CTAs_Score: MetricesCalculation_Data.conversionReport.F.primaryCTA,
+      Forms_Score: MetricesCalculation_Data.conversionReport.F.forms,
+      Thank_You_or_Success_State_Score: MetricesCalculation_Data.conversionReport.F.thankYouState,
+      Tracking_Of_Form_Submits_or_Events_Score: MetricesCalculation_Data.conversionReport.F.tracking,
+      Contact_Info_Score: MetricesCalculation_Data.conversionReport.F.contactInfo,
+      Load_On_CRM_or_Webhook_Score: MetricesCalculation_Data.conversionReport.F.crmWebhook,
+      Conversion_and_Lead_Flow_Score_Total: MetricesCalculation_Data.conversionReport.F.totalFScore,
+    },
+    AIO_Readiness: {
+      Entity_and_Organization_Clarity: {
+        Organization_JSON_LD_Score: MetricesCalculation_Data.aioReport.G.orgFields,
+        Consistent_NAP_Score: MetricesCalculation_Data.aioReport.G.napConsistency,
+        Humans_or_Policies_Score: MetricesCalculation_Data.aioReport.G.policies,
+        Total_Score_G1: MetricesCalculation_Data.aioReport.G.totalG1,
+      },
+      Content_Answerability_and_Structure: {
+        FAQ_or_How_To_JSON_LD_Score: MetricesCalculation_Data.aioReport.G.faqJsonLd,
+        Section_Anchors_or_TOC_Score: MetricesCalculation_Data.aioReport.G.sectionAnchors,
+        Descriptive_Media_Captions_or_Figcaptions_Score: MetricesCalculation_Data.aioReport.G.mediaCaptions,
+        Total_Score_G2: MetricesCalculation_Data.aioReport.G.totalG2,
+      },
+      Product_or_Inventory_Schema_and_Feeds: {
+        Correct_Schema_Types_Score: MetricesCalculation_Data.aioReport.G.productSchemas,
+        Feed_Availability_Score: MetricesCalculation_Data.aioReport.G.feedAvailability,
+        Total_Score_G3: MetricesCalculation_Data.aioReport.G.totalG3,
+      },
+      Crawl_Friendliness_for_Knowledge_Agents: {
+        Robots_Allowlist_Score: MetricesCalculation_Data.aioReport.G.crawlFriendliness,
+        Total_Score_G4: MetricesCalculation_Data.aioReport.G.crawlFriendliness,
+      },
+      AIO_Readiness_Score_Total: MetricesCalculation_Data.aioReport.G.totalGScore,
     }
+  };
 
-    return Metrices
+  return metrices;
 }
